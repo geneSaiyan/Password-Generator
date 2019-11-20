4bc90b047268d62c6f3b59ada4a65bd2ae62d530
Eugene Edwards <eedwards@su.edu>
2019-11-19 22:07:54 -0500

    Adding comments the index.js

--- index.js
+++ index.js
@@ -4,39 +4,46 @@ document.getElementById("btnGenerate").addEventListener("click", function () {
     //Setting password length variable prompt
     var pwdLength = prompt("Choose password length (must be between 8 and 128 characters)");
 
+    //Setting lower/upper character, number, and special character variables
     var lowerChar = 'abcdefghijklmnopqrstuvwxyz';
     var upperChar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     var num = '0123456789';
     var sym = '!@#$%^&*=-_';
+    //Setting generated password variable to the html id password-text
     var generatedPassword = document.getElementById("password-text");
-    var password ="";
+    //Setting empty string password variable
+    var password = "";
 
+    //If password is less than 8 or more than 128 user will get an invalid length alert
     if (pwdLength >= 8 && pwdLength <= 128) {
-    var characters = "";
+        var characters = "";
 
+        //Created a password requirements object to hold all of the user prompts
         var pwdRequirements = {
             pwdSpecialChar: confirm("Would you like a special character?"),
             pwdNumeric: confirm("Would you like a numeric character?"),
             pwdLowerCase: confirm("Would you like a lowercase character?"),
             pwdUpperCase: confirm("Would you like a uppercase character?"),
         }
-      
-        pwdRequirements.pwdSpecialChar == true ? characters += sym: '';
-        pwdRequirements.pwdNumeric == true ? characters += num: '';
-        pwdRequirements.pwdLowerCase == true ? characters += lowerChar: '';
-        pwdRequirements.pwdUpperCase == true ? characters += upperChar: '';
+
+        //Ternary operator used to capture if user prompts are equal to true, if true add the possible characters to the characters variable
+        pwdRequirements.pwdSpecialChar == true ? characters += sym : '';
+        pwdRequirements.pwdNumeric == true ? characters += num : '';
+        pwdRequirements.pwdLowerCase == true ? characters += lowerChar : '';
+        pwdRequirements.pwdUpperCase == true ? characters += upperChar : '';
 
 
-        function password(l,newCharacters){
+        //Password function takes the length and characters
+        function password(l, newCharacters) {
             var pwd = '';
-        for(var i = 0; i<l; i++){
+            for (var i = 0; i < l; i++) {
                 pwd += newCharacters.charAt(Math.floor(Math.random() * newCharacters.length));
+            }
+            return pwd;
         }
-        return pwd;
-    }
 
-    generatedPassword.innerHTML = password(pwdLength, characters);
-        
+        generatedPassword.innerHTML = password(pwdLength, characters);
+
     }
     else {
         alert("Your password does not meet the length requirement!");
